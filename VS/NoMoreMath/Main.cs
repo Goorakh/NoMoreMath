f1aee0e3cd03f2be6bd2c6a6d50ed78f4029d264
a22petoh <petter.ohlen@gmail.com>
2023-02-03 18:19:04 +0100

    Bump version

--- VS/NoMoreMath/Main.cs
+++ VS/NoMoreMath/Main.cs
@@ -11,7 +11,7 @@ namespace NoMoreMath
         public const string PluginGUID = PluginAuthor + "." + PluginName;
         public const string PluginAuthor = "Gorakh";
         public const string PluginName = "NoMoreMath";
-        public const string PluginVersion = "1.0.0";
+        public const string PluginVersion = "1.1.0";
 
         void Awake()
         {
